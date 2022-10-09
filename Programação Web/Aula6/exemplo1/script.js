5da96d68c1f5279751c7e45e5ab4317c3b275e68
ALISON <alisonvieira.av29@gmail.com>
2022-10-09 20:59:39 -0300

    Aula 6 da programação web

---
+++ Programação Web/Aula6/exemplo1/script.js
@@ -0,0 +1,17 @@
+let elemento1;
+let elemento2;
+
+window.onload = () =>{
+
+    elemento1 = document.querySelector("#box1");
+    elemento1.onclick = trocaCor;
+
+
+
+    elemento2 = document.querySelector("#box2");
+
+}
+
+function trocaCor(){
+    
+}
