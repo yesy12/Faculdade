a62ef3f33c7e7091c0ffad3d0eb166eb8452a736
ALISON <alisonvieira.av29@gmail.com>
2022-10-09 21:58:35 -0300

    Exercicio da aula 8

--- Laboratório de Desenvolvimento de Algoritmos/Aula 8/Aula08-Exercicio1/src/main/java/faculdade/aula08/exercicio1/Aula08Exercicio1.java
+++ Laboratório de Desenvolvimento de Algoritmos/Aula 8/Aula08-Exercicio1/src/main/java/faculdade/aula08/exercicio1/Aula08Exercicio1.java
@@ -9,9 +9,44 @@ package faculdade.aula08.exercicio1;
  *
  * @author Alison
  */
-public class Aula08Exercicio1 {
 
+import java.util.Scanner;
+
+public class Aula08Exercicio1 {
+    
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String string, palavra, palavraSubstituida, stringNova;
+        
+        System.out.print("Digite uma frase: ");           
+        Scanner leitura = new Scanner(System.in);
+        string = leitura.nextLine();
+        
+        System.out.print("Digite uma palavra, que será trocada no texto: ");           
+        leitura = new Scanner(System.in);
+        palavra = leitura.nextLine();
+
+        System.out.print("Digite uma palavra para colocar no texto: ");           
+        leitura = new Scanner(System.in);
+        palavraSubstituida = leitura.nextLine();
+        
+        string = string.toLowerCase();
+        palavra = palavra.toLowerCase();
+        
+        stringNova = string.replace(palavra, palavraSubstituida);
+        
+        if(string.equals(stringNova)){
+            System.out.println("Não foi possível alterar a frase");
+            System.out.println("A frase é:{ " +string + " }");
+        }
+        else{
+            System.out.println("Foi possível alterar a frase");
+            System.out.println("A frase Antiga é:{ " +string + " }");
+            System.out.println("A frase Nova é:{ "+stringNova+" }");
+        }
+        
+        
+        
+        
+        
     }
 }
--- Laboratório de Desenvolvimento de Algoritmos/Aula 8/Aula8-Exemplo1/src/main/java/com/mycompany/aula8/exemplo1/Aula8Exemplo1.java
+++ Laboratório de Desenvolvimento de Algoritmos/Aula 8/Aula8-Exemplo1/src/main/java/com/mycompany/aula8/exemplo1/Aula8Exemplo1.java
@@ -1,4 +1,4 @@
-/*
+/*  
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
@@ -16,48 +16,59 @@ import java.util.Scanner;
 public class Aula8Exemplo1 {
 
     public static void main(String[] args) {
-        int num_notas, quantidadeNotas = 0, soma = 0;
+        int num_notas = -1, quantidadeNotas = 0, soma = 0;
         float media,notaAtual = -1;
-        boolean error = false;
+        boolean error = false, passNoError  = false;
         
-        System.out.print("Digite a quantidade de notas para a média: ");
+        System.out.print("\nDigite a quantidade de notas para a média: ");
         Scanner leitura = new Scanner(System.in);
+        
 
-            num_notas = leitura.nextInt();
-               
         
-        
-        do {          
-            do{
-                System.out.print("Digite a "+(quantidadeNotas+1)+"º Nota: ");
-                leitura = new Scanner(System.in);
-                try{
-                    notaAtual = leitura.nextFloat();
-                }
-                catch(InputMismatchException e){
-                    System.out.print("Valor Invalido "+e.toString()+"\n");
-                }
-                
-                
-                if(notaAtual >= 0 && notaAtual < 6){
-                    soma += notaAtual;
-                    error = false;
-                }
-                else{
-                    System.out.print("Valor Invalido: ("+notaAtual+") para a: "+(quantidadeNotas+1)+"º Nota\n");
-                    error = true;
-                }
-                
-            } while(error == true);      
-        
-            quantidadeNotas += 1;
             
-        } while (quantidadeNotas < num_notas);
+            try{
+                num_notas = leitura.nextInt();     
+                passNoError = true;
+            }
+            catch(InputMismatchException e){
+                System.out.print("Valor Invalido "+e.toString()+"\n");
+            }
+            
         
-        media = (float)soma/quantidadeNotas; 
-       
-        System.err.println("A Média é: "+media+", com a quantidade de notas: "+num_notas);
+   
+               
+        if(num_notas > 0 && passNoError){
         
+            do {          
+                do{
+                    System.out.print("Digite a "+(quantidadeNotas+1)+"º Nota: ");
+                    leitura = new Scanner(System.in);
+                    try{
+                        notaAtual = leitura.nextFloat();
+                    }
+                    catch(InputMismatchException e){
+                        System.out.print("Valor Invalido "+e.toString()+"\n");
+                    }
+
+
+                    if(notaAtual >= 0 && notaAtual < 6){
+                        soma += notaAtual;
+                        error = false;
+                    }
+                    else{
+                        System.out.print("Valor Invalido: ("+notaAtual+") para a: "+(quantidadeNotas+1)+"º Nota\n");
+                        error = true;
+                    }
+
+                } while(error == true);      
+
+                quantidadeNotas += 1;
+
+            } while (quantidadeNotas < num_notas && num_notas != 0);
+
+            media = (float)soma/quantidadeNotas; 
+
+            System.out.println("A Média é: "+media+", com a quantidade de notas: "+num_notas);
+        }
     }
 }
-4
