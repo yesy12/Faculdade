e3f18cf55e1ae46e739f43d0352b58184c2d29bd
ALISON <alisonvieira.av29@gmail.com>
2022-10-22 16:24:58 -0300

    prova de Algoritmos

---
+++ Técnicas de Desenvolvimento de Algoritmos/prova/22-10-A2/algoritmo-questao2.c
@@ -0,0 +1,12 @@
+Inicio
+    inteiro base, expoente, potencia;
+
+    escreva("Digite a base: ");
+    leia base;
+
+    escreva("Digite o expoente: ");
+    leia expoente;
+
+    potencia = base**expoente;
+    escreva("A potência é: " +potencia);
+Fim
