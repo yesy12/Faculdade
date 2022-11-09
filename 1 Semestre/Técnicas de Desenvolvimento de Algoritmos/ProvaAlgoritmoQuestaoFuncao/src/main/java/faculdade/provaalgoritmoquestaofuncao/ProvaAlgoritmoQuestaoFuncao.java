17443566511706d66a11a6e7b5e6c46d17e78769
Alison <alisonvieira.av29@gmail.com>
2022-11-09 18:55:59 -0300

    Prova e atividade algoritmo

---
+++ 1 Semestre/Técnicas de Desenvolvimento de Algoritmos/ProvaAlgoritmoQuestaoFuncao/src/main/java/faculdade/provaalgoritmoquestaofuncao/ProvaAlgoritmoQuestaoFuncao.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package faculdade.provaalgoritmoquestaofuncao;
+
+/**
+ *
+ * @author Alison
+ */
+public class ProvaAlgoritmoQuestaoFuncao {
+
+    public static void main(String[] args) {
+  int x, y, total; 
+
+    x = 10;
+
+    y = 20;
+
+
+    metodo1(x);
+
+
+    total = x + y;
+
+        System.out.print(total);
+
+        
+    }
+    
+        static void metodo1(int x)
+
+{
+
+    int y = 5;
+
+    x = x + y;
+
+}
+
+}
